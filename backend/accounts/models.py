2a0748c20dce214b73a1a16f68ac47320e6a1f5e
Rakshith0907 <rakshithrakki003@gmail.com>
2026-05-08 23:12:21 +0530

    Added custom user model with role field in accounts app

---
+++ backend/accounts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        EMPLOYEE = 'employee', 'Employee'
+        MANAGER = 'manager', 'Manager'
+        ADMIN = 'admin', 'Admin'
+    role = models.CharField(max_length=20, choices=Role.choices, default=Role.EMPLOYEE)
