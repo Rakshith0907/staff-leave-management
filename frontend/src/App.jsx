8a923d1d87f9785de80616b300eaa7accc180c62
Rakshith0907 <rakshithrakki003@gmail.com>
2026-05-14 18:18:06 +0530

    add navbar with role based navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { AuthProvider } from './context/AuthContext'
-import { Route, Routes, Navigate } from 'react-router-dom'
+import { Route, Routes, Navigate, Outlet } from 'react-router-dom'
 import Login from './pages/Login'
 import PrivateRoute from './components/PrivateRoute'
 import Dashboard from './pages/Dashboard'
@@ -8,18 +8,31 @@ import ApplyLeave from './pages/ApplyLeave'
 import MyLeaves from './pages/MyLeaves'
 import ManageLeaves from './pages/ManageLeaves'
 import AdminPanel from './pages/AdminPanel'
+import Navbar from './components/Navbar'
 
 const App = () => {
+
+  const PrivateLayout = () => {
+    return (
+        <>
+            <Navbar />
+            <Outlet />
+        </>
+    )
+  }
+
   return (
     <AuthProvider>
       <Routes>
-        <Route path='/login' element={<Login />} />
+        <Route path='/login' element={<Login />} /> 
         <Route path='/' element={<PrivateRoute />}>
-          <Route index element={<Dashboard />} />
-          <Route path='apply' element={<ApplyLeave />} />
-          <Route path='my-leaves' element={<MyLeaves />} />
-          <Route path='manage-leaves' element={<ManageLeaves />} />
-          <Route path='admin' element={<AdminPanel />} />
+          <Route element={<PrivateLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path='apply' element={<ApplyLeave />} />
+            <Route path='my-leaves' element={<MyLeaves />} />
+            <Route path='manage-leaves' element={<ManageLeaves />} />
+            <Route path='admin' element={<AdminPanel />} />
+          </Route>
         </Route>
         <Route path='*' element={<Navigate to='/login' /> }  />
       </Routes>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
+import { useAuth } from "../context/AuthContext";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { user, logout } = useAuth();
+  const navigate = useNavigate()
 
-export default Navbar
+
+  const handleLogout = () =>{
+    logout()
+    navigate('/login')
+  }
+
+  return (
+    <div className="nav-main">
+      <div className="nac-container">
+        <Link to="/">Dashboard</Link>
+        {user.role === "employee" && (
+          <>
+            <Link to="apply">ApplyLeave</Link>
+            <Link to="my-leaves">My Leaves</Link>
+          </>
+        )}
+        {user.role === 'manager' && (
+          <>
+            <Link to='manage-leaves' >Manage Leaves</Link>
+          </>
+        )}
+        {user.role === 'admin' && (
+          <>
+            <Link to='admin' >Admin Panel</Link>
+          </>
+        )}
+        <button className="btn btn-danger" onClick={handleLogout}>Logout</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
