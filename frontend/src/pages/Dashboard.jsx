96a603a93656e255af6c0d6ebcba6fd5d154ddd4
Rakshith0907 <rakshithrakki003@gmail.com>
2026-05-15 14:01:18 +0530

    Completed dashboard welcome message.

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useAuth } from "../context/AuthContext";
 
 const Dashboard = () => {
+  const { user } = useAuth()
+
   return (
     <div>
-      Dashboard
+      <h2>Welcome! {user.username}</h2>
+      {user.role==='employee' && (<p>You can apply for leaves</p>)}
+      {user.role==='manager' && (<p>Please check for new leave requests and take an action(acccept or reject)</p>)}
+      {user.role==='admin' && (<p>check all the leave applications.</p>)}
     </div>
   )
 }
