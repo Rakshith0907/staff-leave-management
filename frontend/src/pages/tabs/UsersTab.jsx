f8b75b88cd1a8496cd7ce3734cc254070e4db462
Rakshith0907 <rakshithrakki003@gmail.com>
2026-05-18 21:08:00 +0530

    Added usersTab.

--- frontend/src/pages/tabs/UsersTab.jsx
+++ frontend/src/pages/tabs/UsersTab.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { use, useEffect, useState } from "react";
+import api from "../../api/axios";
+import { FaUserCircle } from "react-icons/fa";
 
 const UsersTab = () => {
-  return (
-    <div>
-      UsersTab
-    </div>
-  )
-}
+  const [users, setUsers] = useState([]);
 
-export default UsersTab
+  const getUsers = async () => {
+    const responseData = await api.get("auth/users/");
+    setUsers(responseData.data);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const groupByDepartment = (users) => {
+    return users.reduce((groups, user) => {
+      const dept = user.department?.name || "No Department";
+      if (!groups[dept]) groups[dept] = [];
+      groups[dept].push(user);
+      return groups;
+    }, {});
+  };
+
+  users.sort((a, b) => (a.role === "manager" ? -1 : 1));
+
+  const groups = groupByDepartment(users);
+
+  return (
+    <div className="users-main">
+      <div className="users-container">
+        {Object.entries(groups).map(([deptName, users]) => {
+          
+          return (
+            <div key={deptName} className="user-group">
+              <h3>{deptName}</h3>
+              {users.map((user) => {
+                console.log(user)
+                return (
+                  <div key={user.id} className="user-card">
+                    <FaUserCircle />
+                    <h4>{user.username}</h4>
+                    <p>{user.email}</p>
+                    <p>{user.role}</p>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default UsersTab;
