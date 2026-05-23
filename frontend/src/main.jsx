d1fec2cac5e1f05ee0ca089ee41b6980ec596e34
Rakshith0907 <rakshithrakki003@gmail.com>
2026-05-23 23:50:20 +0530

    Added styles folder with variable file with defined colors

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./styles/variables.css"
 
 
 createRoot(document.getElementById('root')).render(
